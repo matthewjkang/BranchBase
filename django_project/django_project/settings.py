b53dbfa8dc414ace3350af46a091195c524eddf4
Matthew J. Kang <mjk095@ucsd.edu>
2022-05-31 14:27:34 -0700

    secret key is now hidden

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p9xal%8k*!0h8mvg%x9nl!78#3ym-c$rve*h@@8p)0a#yez9i)'
-
+#SECRET_KEY = 'django-insecure-p9xal%8k*!0h8mvg%x9nl!78#3ym-c$rve*h@@8p)0a#yez9i)'
+SECRET_KEY  = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
